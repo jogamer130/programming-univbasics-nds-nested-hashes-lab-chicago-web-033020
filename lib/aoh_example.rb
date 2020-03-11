7187a1dae7941318e8e022ef47efb646855bda70
Derek Williams <jogamer130@gmail.com>
2020-03-11 02:38:41 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,8 +14,8 @@ def assembled_aoh
     {:name => "Don Gately", :occupation => "Live-in Staff"}, 
     {:name => "Joelle van Dyne", :occupation =>"Radio Personality"}, 
     {:name => "Pat Monteseian", :occupation => "Staff"}, 
-    {:name => "Kate Gompert" :occupation => "None"} 
-    {:name "Bruce Green", :occupation => "Fan of Mildred"}
+    {:name => "Kate Gompert", :occupation => "None"} 
+    {:name => "Bruce Green", :occupation => "Fan of Mildred"}
     ]
   
 end
